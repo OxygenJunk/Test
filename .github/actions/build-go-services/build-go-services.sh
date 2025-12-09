470a83e6436a9f44c9baf0cc40cca4d31cba1919
Matej Umiljanović <matejumiljanovic@gmail.com>
2025-12-09 18:00:04 +0100

    test

--- .github/actions/build-go-services/build-go-services.sh
+++ .github/actions/build-go-services/build-go-services.sh
@@ -10,14 +10,13 @@ while IFS= read -r line; do
     output=$(go build -o $BUILD_DIR -buildvcs=false  . 2>&1) 
     # $? je exit status od zadnje komande sto je go build iznad
     if [[ $? -ne 0 ]]; then # -ne znaci not equal to -- 0 je valjda success za go build uvijek
-      payload='{"text":"'$SERVICE_NAME' Go build failed with error: '$output'"}'
-      echo ${payload}
-    #   curl -X POST -H 'Content-type: application/json' --data "$payload" $SLACK_WEBHOOK_URL
+      # payload='{"text":"'$SERVICE_NAME' Go build failed with error: '$output'"}'
+      # echo ${payload}
+      # curl -X POST -H 'Content-type: application/json' --data "$payload" $SLACK_WEBHOOK_URL
       echo "Build failed - ${SERVICE_NAME} - ${output}"
       exit 1
-    else
-      echo "Build succeeded - ${SERVICE_NAME}"
     fi
+    echo "Build succeeded - ${SERVICE_NAME}"
     cd ..
   done
   cd $WD
