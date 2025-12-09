67e2f982a14098a60fa5b856deae258e306ea959
Matej Umiljanović <matejumiljanovic@gmail.com>
2025-12-09 17:32:07 +0100

    test

--- .github/actions/build-go-services/build-go-services.sh
+++ .github/actions/build-go-services/build-go-services.sh
@@ -1,5 +1,6 @@
 CMD_DIRS=$(find . -name "cmd" -type d)
 WD=$(pwd)
+set -e
 while IFS= read -r line; do
   cd $line
   for d in */ ; do
@@ -13,7 +14,6 @@ while IFS= read -r line; do
     #   payload='{"text":"'$SERVICE_NAME' Go build failed with error: '$output'"}'
     #   curl -X POST -H 'Content-type: application/json' --data "$payload" $SLACK_WEBHOOK_URL
       echo "Build failed - ${SERVICE_NAME} - ${output}"
-      exit 1
     else
       echo "Build succeeded - ${SERVICE_NAME}"
     fi
