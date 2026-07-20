16eb0e3d4acb143119f7dc6cceaec6b1140ca22c
Matej Umiljanović <matejumiljanovic@gmail.com>
2026-07-20 18:54:10 +0200

    test

--- .github/actions/build-go-services/build-go-services.sh
+++ .github/actions/build-go-services/build-go-services.sh
@@ -8,7 +8,7 @@ while IFS= read -r line; do
     BUILD_DIR=$GITHUB_WORKSPACE/build/$SERVICE_NAME/bin
     mkdir -p $BUILD_DIR
     echo "${GOARCH}"
-    go env GOOS GOARCH
+    go env GOOS GOARCH GOAMD64
     output=$(go build -o $BUILD_DIR -buildvcs=false  . 2>&1) 
     # $? je exit status od zadnje komande sto je go build iznad
     if [[ $? -ne 0 ]]; then # -ne znaci not equal to -- 0 je valjda success za go build uvijek
