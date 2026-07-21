ffeac29ab1132d32353e6f06216c206063f29c40
Matej Umiljanović <matejumiljanovic@gmail.com>
2026-07-21 14:39:33 +0200

    test

--- .github/actions/build-go-services/build-go-services.sh
+++ .github/actions/build-go-services/build-go-services.sh
@@ -34,33 +34,17 @@ while IFS= read -r line; do
     echo "Build succeeded - ${SERVICE_NAME}"
     file "$BUILD_DIR"/*
     cd ..
+    BINARY_PATH=$(find "$BUILD_DIR" -type f -name "$SERVICE_NAME" | head -n 1)
+
+    if [ -z "$BINARY_PATH" ]; then
+      echo "Error: Could not find "$SERVICE_NAME" binary inside $BUILD_DIR" >&2
+      exit 1
+    fi
+
+    echo "=== Found binary at: $BINARY_PATH ==="
+
+    # Run the metadata inspection command
+    go version -m "$BINARY_PATH" | grep -E "GOARCH|GOAMD64|CGO_ENABLED" || echo "Using system defaults"
   done
   cd $WD
 done <<< "$CMD_DIRS"
-
-# Dynamically find the 'first_puzzle' binary inside the build directory
-BINARY_PATH=$(find "$BUILD_DIR" -type f -name "first_puzzle" | head -n 1)
-
-if [ -z "$BINARY_PATH" ]; then
-  echo "Error: Could not find first_puzzle binary inside $BUILD_DIR" >&2
-  exit 1
-fi
-
-echo "=== Found binary at: $BINARY_PATH ==="
-
-# Run the metadata inspection command
-go version -m "$BINARY_PATH" | grep -E "GOARCH|GOAMD64|CGO_ENABLED" || echo "Using system defaults"
-
-
-# Dynamically find the 'main' binary inside the build directory
-BINARY_PATH=$(find "$BUILD_DIR" -type f -name "main" | head -n 1)
-
-if [ -z "$BINARY_PATH" ]; then
-  echo "Error: Could not find main binary inside $BUILD_DIR" >&2
-  exit 1
-fi
-
-echo "=== Found binary at: $BINARY_PATH ==="
-
-# Run the metadata inspection command
-go version -m "$BINARY_PATH" | grep -E "GOARCH|GOAMD64|CGO_ENABLED" || echo "Using system defaults"
